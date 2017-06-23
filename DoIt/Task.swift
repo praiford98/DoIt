0e758b763f0efc73e1c18b3dd5b92ef22efa0505
Patrick Raiford <praiford98@gmail.com>
2017-06-23 15:22:12 -0400

    First update

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Patrick Raiford on 6/23/17.
+//  Copyright © 2017 Patrick Raiford. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
